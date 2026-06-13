7d2b8d5cf662d20cac41e837c0a2118519be1b1a
sunil <sunilpail16@gmail.com>
2026-06-13 01:24:43 +0100

    Add hypr config

---
+++ hypr/custom/general.lua
@@ -0,0 +1,15 @@
+hl.monitor({ output = "DP-3", mode = "2560x1440@144Hz", position = "0x0", scale = 1 })
+hl.monitor({ output = "DP-2", mode = "2560x1440@144Hz", position = "2560x-560", scale = 1, transform = 3 })
+
+-- workspace(1, { monitor = "DP-3", default = true })
+-- workspace(2, { monitor = "DP-2" })
+-- hl.workspace_rule({ workspace = "1", monitor = "DP-3", default = true ,default_name = "slider" })
+hl.workspace_rule({ workspace = "2", monitor = "DP-2" })
+hl.workspace_rule({ workspace = "1", monitor = "DP-3", default = true })
+
+
+hl.config({
+    input = {
+        kb_layout = "gb"
+    }
+})
---
+++ hypr/custom/keybinds.lua
@@ -0,0 +1,2 @@
+hl.bind("SUPER + Grave", hl.dsp.exec_cmd(terminal))
+hl.bind("Print", hl.dsp.global("quickshell:regionScreenshot"), {description = "Screen snip"} )
