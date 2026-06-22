240fa3b071e7815ed5b1ec2ba8fa87f5f93b0ffb
sunil <sunilpail16@gmail.com>
2026-06-22 21:59:32 +0100

    Add Hyprland configuration updates and keybindings

---
+++ hypr/custom/execs.lua
@@ -0,0 +1,3 @@
+hl.exec_cmd("/usr/lib/xdg-desktop-portal-hyprland")
+hl.exec_cmd("/usr/lib/xdg-desktop-portal-gtk")
+hl.exec_cmd("sleep 1 && /usr/lib/xdg-desktop-portal")
--- hypr/custom/keybinds.lua
+++ hypr/custom/keybinds.lua
@@ -1,2 +1,5 @@
 hl.bind("SUPER + Grave", hl.dsp.exec_cmd(terminal))
 hl.bind("Print", hl.dsp.global("quickshell:regionScreenshot"), {description = "Screen snip"} )
+
+
+hl.bind("SUPER + Z", hl.dsp.exec_cmd(browser2))
